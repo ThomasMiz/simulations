e94244ea1c26af270ee4484024b0b19f5d0edf62
Thomas Mizrahi <32400648+ThomasMiz@users.noreply.github.com>
2025-03-14 12:55:00 -0300

    Repeat tests 100 times, add stdev

---
+++ SimulationBase/LinqExtensions.cs
@@ -0,0 +1,13 @@
+namespace SimulationBase;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class LinqExtensions
+{
+    public static double StandardDeviation(this IEnumerable<double> values)
+    {
+        double avg = values.Average();
+        return Math.Sqrt(values.Average(v=>Math.Pow(v-avg,2)));
+    }
+}
--- tp1/ProgramGraphs.cs
+++ tp1/ProgramGraphs.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using SimulationBase;
 using tp1.Simulate;
 
 namespace tp1;
@@ -11,10 +15,11 @@ public static class ProgramGraphs
     public static void RunSimulations()
     {
         CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-        
+
         Console.WriteLine("Starting up...");
 
         int[] MValues = { 1, 2, 3, 4, 8, 12, 16, 20, 24 };
+        const int TRIES = 100;
         string outputFile = "simulation_results.txt";
 
         using (StreamWriter writer = new StreamWriter(outputFile))
@@ -23,19 +28,28 @@ public static class ProgramGraphs
 
             foreach (int m in MValues)
             {
-                var config = SimulationConfig.FromFile("Examples/Static100.txt");
-                config.M = m;
+                int count = 0;
+                List<double> elapseds = new();
+                for (int i = 0; i < TRIES; i++)
+                {
+                    var config = SimulationConfig.FromFile("Examples/Static100.txt");
+                    count = config.Particles.Count;
+                    config.M = m;
 
-                var simulation = new Simulation(config);
+                    var simulation = new Simulation(config);
 
-                Stopwatch stopwatch = Stopwatch.StartNew();
-                simulation.Initialize();
-                stopwatch.Stop();
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    simulation.Initialize();
+                    stopwatch.Stop();
 
-                TimeSpan elapsedTime = stopwatch.Elapsed;
+                    TimeSpan elapsedTime = stopwatch.Elapsed;
+                    elapseds.Add(elapsedTime.TotalMilliseconds);
+                }
 
-                writer.WriteLine($"{config.Particles.Count}\t{m}\t{elapsedTime.TotalMilliseconds}");
-                Console.WriteLine($"Simulation with M={m} completed in {elapsedTime.TotalMilliseconds} ms.");
+                double average = elapseds.Average();
+                double stdev = elapseds.StandardDeviation();
+                Console.WriteLine($"Simulation with M={m} completed in average {average} ms.");
+                writer.WriteLine($"{count}\t{m}\t{average}\t{stdev}");
             }
         }
 
