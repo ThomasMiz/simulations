76cf8fbb7599e35f42bb0eed2288417e3435e2d0
Thomas Mizrahi <32400648+ThomasMiz@users.noreply.github.com>
2025-03-24 19:03:15 -0300

    Add from_frame, to_frame, skip_frame and interval params to animator

--- tp2/animador.py
+++ tp2/animador.py
@@ -2,6 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+# Los índices de los fotogramas a mostrar (inclusive ambos)
+from_frame = 0
+to_frame = None
+
+# No dibujar todos los fotogramas, mostrar por ej uno de cada 10 (desactivar con skip_frames=1)
+skip_frames = 1
+
+# Tiempo entre fotogramas
+interval = 1
+
 # Parámetros de la grilla
 N = 50                    # Tamaño de la grilla (debe coincidir con el usado en la simulación)
 input_file = "./bin/Debug/net8.0/output.txt"  # Archivo generado por simulador.py
@@ -9,21 +19,24 @@ input_file = "./bin/Debug/net8.0/output.txt"  # Archivo generado por simulador.p
 # Leer las grillas desde el archivo
 grids = []
 with open(input_file, "r") as f:
+    frame_index = 0
     for line in f:
-        flat = np.array(list(map(int, line.strip().split())))  # Usa split() para manejar espacios
-        grid = flat.reshape((N, N))
-        grids.append(grid)
+        if frame_index >= from_frame and (to_frame is None or frame_index <= to_frame) and (frame_index - from_frame) % skip_frames == 0:
+            flat = np.array(list(map(int, line.strip().split())))  # Usa split() para manejar espacios
+            grid = flat.reshape((N, N))
+            grids.append((frame_index, grid))
+        frame_index += 1
 
 # Crear la animación
 fig, ax = plt.subplots()
-im = ax.imshow(grids[0], cmap='gray', vmin=-1, vmax=1)  # Ajuste de colores para {-1,1}
+im = ax.imshow(grids[0][1], cmap='gray', vmin=-1, vmax=1)  # Ajuste de colores para {-1,1}
 
 def update(frame):
-    im.set_data(grids[frame])
-    ax.set_title(f"Paso {frame}")
+    im.set_data(grids[frame][1])
+    ax.set_title(f"Paso {grids[frame][0]}")
     return [im]
 
-ani = animation.FuncAnimation(fig, update, frames=len(grids), interval=1, blit=True)
+ani = animation.FuncAnimation(fig, update, frames=len(grids), interval=interval, blit=True)
 
 plt.tight_layout()
 plt.show()
