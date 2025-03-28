54cb0df96e9a3a9b02fea40f2d36aef60407c4eb
boullo <jboullosagutierrez@itba.edu.ar>
2025-03-28 12:56:47 -0300

    item b
    Co-Authored-By: abrilocchipinti <75281352+abrilocchipinti@users.noreply.github.com>
    Co-Authored-By: ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>

---
+++ tp2/ProgramB.cs
@@ -0,0 +1,38 @@
+using System.Globalization;
+
+namespace tp2;
+
+public class ProgramB
+{
+    public static void MakeGraphs()
+    {
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+        Console.WriteLine("Starting up simulation...");
+
+        List<float> probabilityValues = [0.01f, 0.1f, 0.9f];
+
+        Parallel.ForEach(probabilityValues, (probability =>
+        {
+            var config = new SimulationConfig()
+            {
+                GridFile = "data/semilla.txt",
+                Probability = probability,
+                RandomSeed = 1234,
+                MaxSteps = 10000,
+                ConsensusEpsilon = 0,
+                StationaryEpsilon = 0,
+                OutputFile = $"output-b-{probability}.txt",
+                ConsensoFile = $"consenso-b-{probability}.txt",
+                ClusterStatsFile = $"clusterstats-b-{probability}.txt",
+            };
+
+            using var simulation = config.Build();
+
+            simulation.Run();
+        }));
+
+
+        Console.WriteLine("Goodbye :-)");
+    }
+}
